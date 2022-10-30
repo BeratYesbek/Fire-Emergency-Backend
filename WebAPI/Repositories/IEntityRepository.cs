10852dfcd12c5c5868aa06c0aedbc72042740f24
Feanor <beratyesbek@gmail.com>
2022-10-30 18:29:06 +0300

    EntityRepository Created

---
+++ WebAPI/Repositories/IEntityRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+using WebAPI.Entities.abstracts;
+
+namespace WebAPI.Repositories
+{
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        T Add(T entity);
+        T Update(T entity);
+        void Delete(T entity);
+        T Get(Expression<Func<T, bool>> filter);
+        List<T> GetAll(Expression<Func<T, bool>>? filter = null, params Expression<Func<T, object>>[] including);
+    }
+}
