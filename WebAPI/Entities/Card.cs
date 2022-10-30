62552f5877f46ce404e12afd89e94c140893468b
Feanor <beratyesbek@gmail.com>
2022-10-30 14:06:20 +0300

    Entities Created

---
+++ WebAPI/Entities/Card.cs
@@ -0,0 +1,12 @@
+﻿using WebAPI.Entities.abstracts;
+
+namespace WebAPI.Entities
+{
+    public class Card : IEntity
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
--- WebAPI/Entities/User.cs
+++ WebAPI/Entities/User.cs
@@ -5,13 +5,11 @@ namespace WebAPI.Entities
     public class User : IEntity
     {
         public int Id { get; set; }
-
         public string? Email { get; set; }
-
         public string? Username { get; set; }
-
         public string? PhoneNumber { get; set; }
-        public byte[]? PasswordSalt { get; set; }
-        public byte[]? PasswordHash { get; set; }
+       /* public byte[]? PasswordSalt { get; set; }
+        public byte[]? PasswordHash { get; set; }*/
+        public virtual List<User>? Users { get; set; }
     }
 }
